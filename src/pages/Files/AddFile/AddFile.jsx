0972d816a8b602d6a123df0aec5e005dc80cb2cf
Pedro Manga <pedroblack1504@gmail.com>
2024-04-30 17:02:01 +0200

    formulario para añadir arachivos funcionando correctamente, falta añadir el campo playlist

--- src/pages/Files/AddFile/AddFile.jsx
+++ src/pages/Files/AddFile/AddFile.jsx
@@ -2,33 +2,79 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import FormLabel from 'react-bootstrap/esm/FormLabel';
 import { CreateFile } from '../../../services/apiCalls';
+import { useState } from 'react';
 
 
 export const AddFiles = () => {
+    const [formData, setFormData] = useState({
+        nombre: '',
+        descripcion: '',
+        ubicacion: '',
+        duracion: 0,
+        archivo: undefined
+
+    });
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value })
+    }
+
+    const handleFileChange = (event) => {
+        const selectedFile = event.target.files[0]
+        console.log(selectedFile)
+        setFormData({
+            ...formData,
+            archivo: selectedFile,
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        //Envia los dato del formulario al CreateFile
+        CreateFile(formData)
+            .then((response) => {
+                console.log(response);
+                //limpia el formulario
+                setFormData({
+                    nombre: '',
+                    descripcion: '',
+                    duracion: '',
+                    duracion: '',
+                    archivo: null
+                });
+                <div class="alert alert-success" role="alert">
+                    Archivo creado correctamente
+                </div>
+            })
+            .catch((error) => {
+                console.error("Error añadir el archivo", error)
+            });
+    };
+
     return (
         <div className='border border-success'>
-            <Form className='w-75 p-3' action={CreateFile(FormData)} >
-                <legend>INFOTOOLS</legend>
+            <Form className='w-75 p-3' onSubmit={handleSubmit} >
+                <legend>FORMULARIO PARA AÑADIR ARCHIVOS</legend>
                 <fieldset></fieldset>
                 <Form.Group className='mb-3' controlId='nombre'>
-                   <Form.Label>Nombre del archivo</Form.Label>
-                   <Form.Control type='text'></Form.Control>
+                    <Form.Label>Nombre del archivo</Form.Label>
+                    <Form.Control type='text' value={formData.nombre} onChange={handleChange} name='nombre' required></Form.Control>
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='descripcion'>
-                   <Form.Label>Descripcion</Form.Label>
-                   <Form.Control type='text'></Form.Control>
+                    <Form.Label>Descripcion</Form.Label>
+                    <Form.Control type='text' name='descripcion' value={formData.descripcion} onChange={handleChange} ></Form.Control>
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='ubicacion'>
-                   <Form.Label>Ubicacion</Form.Label>
-                   <Form.Control type='text'></Form.Control>
+                    <Form.Label>Ubicacion</Form.Label>
+                    <Form.Control type='text' name='ubicacion' value={formData.ubicacion} onChange={handleChange}></Form.Control>
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='duracion'>
-                   <Form.Label>Duracion</Form.Label>
-                   <Form.Control type='number'></Form.Control>
+                    <Form.Label>Duracion</Form.Label>
+                    <Form.Control type='number' name='duracion' value={formData.duracion} onChange={handleChange} placeholder='Indique la duración en caso de ser una imagen' required></Form.Control>
                 </Form.Group>
                 <Form.Group className='mb-3' controlId='archivo'>
-                   <Form.Label>Seleciona un archivo</Form.Label>
-                   <Form.Control type='file'></Form.Control>
+                    <Form.Label>Seleciona un archivo</Form.Label>
+                    <Form.Control type='file' name='archivo' onChange={handleFileChange}></Form.Control>
                 </Form.Group>
                 <Button type="submit">Crear archivo</Button>
             </Form>
--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -5,37 +5,49 @@ import axios from 'axios'
  */
 const urlBase = 'http://localhost:3000'
 export const fetchFiles = async () => {
-    try {
-      const response = await axios.get('http://localhost:3000/files');
-      console.log(JSON.stringify(response.data));
-      return response.data; // Devuelve los datos para su uso en React
-    } catch (error) {
-      console.log(error);
-      throw error; // Maneja el error o lanza una excepción para que lo maneje el componente que llama a fetchFiles
-    }
+  try {
+    const response = await axios.get('http://localhost:3000/files');
+    console.log(JSON.stringify(response.data));
+    return response.data; // Devuelve los datos para su uso en React
+  } catch (error) {
+    console.log(error);
+    throw error; // Maneja el error o lanza una excepción para que lo maneje el componente que llama a fetchFiles
   }
+}
 
-  /**
- * Realiza una solicitud para obtener los detalles de un archivo por su ID.
- * @param {string} id - ID del archivo para la cual se solicitan los detalles.
- * @returns {Promise} Promise que se resuelve con los datos de la respuesta.
- */
-  export const GetFileById= async(id)=>{
-    try {
-      const response = await axios.get(`${urlBase}/file/${id}`)
-      console.log(JSON.stringify(response.data))
-      return response.data;
-    } catch (error) {
-      
-    }
-  }
+/**
+* Realiza una solicitud para obtener los detalles de un archivo por su ID.
+* @param {string} id - ID del archivo para la cual se solicitan los detalles.
+* @returns {Promise} Promise que se resuelve con los datos de la respuesta.
+*/
+export const GetFileById = async (id) => {
+  try {
+    const response = await axios.get(`${urlBase}/file/${id}`)
+    console.log(JSON.stringify(response.data))
+    return response.data;
+  } catch (error) {
 
-  export const CreateFile = async(formData)=>{
-    try {
-      console.log(formData)
-      
-      
-    } catch (error) {
-      
-    }
   }
+}
+
+export const CreateFile = async (ClientformData) => {
+  try {
+    // Se crea un objeto FormData que enviaremos al servidor
+    console.log(ClientformData)
+    const response = await axios.postForm(`${urlBase}/file`, {
+      nombre: ClientformData.nombre,
+      ubicacion: ClientformData.ubicacion,
+      descripcion: ClientformData.descripcion,
+      archivo: ClientformData.archivo
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data' // Es importante establecer el tipo de contenido como 'multipart/form-data' para enviar archivos
+      }
+    })
+    console.log(response)
+    return response
+
+  } catch (error) {
+    console.error(error)
+  }
+}
